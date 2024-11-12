729ae8ab34782227971dfadf66878bdd08ca4870
Tristan Golding <tristangolding2002@gmail.com>
2024-11-12 22:11:33 +0000

    remove whitenoise caching

--- infralign/settings.py
+++ infralign/settings.py
@@ -143,9 +143,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
 # and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-WHITENOISE_ADD_HEADERS_FUNCTION = None
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
