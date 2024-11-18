a315c4e52036ebd7cdef288b9d6c06da5c7cc4d5
Tristan Golding <tristangolding2002@gmail.com>
2024-11-18 23:11:55 +0000

    added projects page w add & del

--- analytics/urls.py
+++ analytics/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('projects', views.projects, name='projects'),
+    path('projects/new', views.add_project, name='add_project'),
+    path('projects/<int:pk>', views.project, name='project'),
+    path('projects/<int:project_pk>/delete', views.delete_project, name='delete-project'),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+import logging
+
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.contrib.postgres.search import SearchVector
 
 from .models import AutodeskConstructionCloudProject
+from .forms import AutodeskConstructionCloudProjectForm
+
+logger = logging.getLogger(__name__)
+
+def verbose_user(request) -> str:
+    """Returns a unique string representing the user within the request. Meant for logging purposes."""
+    return f"{request.user.pk}:{request.user.username}"
 
 @login_required
 def index(request):
@@ -12,4 +22,35 @@ def projects(request):
     if not request.user.has_perm('analytics.view_autodeskconstructioncloudproject'):
         raise PermissionDenied
     can_add_projects = True if request.user.has_perm('analytics.add_autodeskconstructioncloudproject') else False
-    return render(request, 'projects.html', {'can_add_projects': can_add_projects, 'projects': AutodeskConstructionCloudProject.objects.all()})
+    if request.GET.get('search'):
+        p = AutodeskConstructionCloudProject.objects.annotate(search=SearchVector('name', 'org'),).filter(search=request.GET.get('search'))
+    else:
+        p = AutodeskConstructionCloudProject.objects.all()
+    return render(request, 'projects.html', {'can_add_projects': can_add_projects, 'projects': p})
+
+def project(request, pk):
+    if not request.user.has_perm('analytics.view_autodeskconstructioncloudproject'):
+        raise PermissionDenied
+    return render(request, 'project.html', {'project': AutodeskConstructionCloudProject.objects.get(pk=pk)})
+
+def add_project(request):
+    if not request.user.has_perm('analytics.add_autodeskconstructioncloudproject'):
+        raise PermissionDenied
+    if request.method == "POST":
+        print(request.POST)
+        form = AutodeskConstructionCloudProjectForm(data=request.POST)
+        print(form.errors)
+        if form.is_valid():
+            proj = form.save()
+            return redirect('projects')
+    else:
+        form = AutodeskConstructionCloudProjectForm()
+    return render(request, "add_project.html", {"form": form})
+
+def delete_project(request, project_pk):
+    """Django view for deleting a Software model."""
+    proj = AutodeskConstructionCloudProject.objects.get(pk=project_pk)
+    proj.delete()
+    logger.info(f"{verbose_user(request)} sucessfully deleted {proj.pk}:{proj.name}.")
+    # messages.success(request, f"{software} was deleted successfully.")
+    return redirect('projects')
