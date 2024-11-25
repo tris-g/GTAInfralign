8ebcf051ad601d757287a6f42f9d3a509c88c961
Tristan Golding <tristangolding2002@gmail.com>
2024-11-25 17:37:04 +0000

    debug

--- infralign/settings.py
+++ infralign/settings.py
@@ -29,7 +29,8 @@ import dj_database_url
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+# DEBUG = "RENDER" not in os.environ
+DEBUG = True
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
