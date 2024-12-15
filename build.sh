658d4708d5248fe3b558a1d92b09dc1a7032d079
Tristan Golding <tristangolding2002@gmail.com>
2024-12-15 00:56:14 +0000

    collectstatic after coverage test

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic --no-input
-
 # Apply any outstanding database migrations
 python manage.py migrate
 
 # Create coverage files for dynamic stats
 coverage run manage.py test
 coverage json -o $PWD/static/testing/coverage.json
+
+# Convert static asset files
+python manage.py collectstatic --no-input
