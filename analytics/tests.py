6a627d9452e6fdbe8a3ba003f7a366f8899813b8
Tristan Golding <tristangolding2002@gmail.com>
2024-12-19 12:54:39 +0000

    testing additions and seperation

--- analytics/tests.py
+++ analytics/tests.py
@@ -66,7 +66,8 @@ class TestAnalytics(TestCase):
         self.assertIn('projects_list', response.context)
         self.assertEqual(list(response.context['projects_list']), list(AutodeskConstructionCloudProject.objects.all()))
 
-        # Test search function
+    def test_view_projects_search(self):
+        self.client.force_login(self.good_user)
         search_response = self.client.get(reverse('view_all_projects'), {'search': 'Test'})
         self.assertEqual(search_response.status_code, 200)
         self.assertContains(search_response, self.project.name)
@@ -77,8 +78,9 @@ class TestAnalytics(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('reports_list', response.context)
         self.assertEqual(list(response.context['reports_list']), list(AutodeskConstructionCloudReport.objects.all()))
-
-        # Test search function
+    
+    def test_view_reports_search(self):
+        self.client.force_login(self.good_user)
         search_response = self.client.get(reverse('view_all_reports'), {'search': 'Test'})
         self.assertEqual(search_response.status_code, 200)
         self.assertContains(search_response, self.report.name)
@@ -88,14 +90,13 @@ class TestAnalytics(TestCase):
         response = self.client.get(reverse('add_project'))
         self.assertEqual(response.status_code, 200)
         
+    def test_add_project_post(self):
         response = self.client.post(reverse('add_project'), {'name': 'Good project', 'org': 'Good organisation'})
-        self.assertEqual(response.status_code, 302)  # Expecting a redirect after success
-
-        # Check that the book was created in the database
-        self.assertEqual(AutodeskConstructionCloudProject.objects.count(), 2)  # Now two projects should exist
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(AutodeskConstructionCloudProject.objects.count(), 2)
         self.assertTrue(AutodeskConstructionCloudProject.objects.filter(name='Good project', org='Good organisation').exists())
         
-        self.client.logout()
+    def test_add_project_post_bad(self):
         view_permission = Permission.objects.get(codename='view_autodeskconstructioncloudproject', content_type__app_label='analytics')
         self.bad_user.user_permissions.add(view_permission)
         self.client.force_login(self.bad_user)
@@ -108,6 +109,9 @@ class TestAnalytics(TestCase):
         response = self.client.get(reverse('add_report'))
         self.assertEqual(response.status_code, 200)
 
+    def test_add_report_post(self):
+        self.client.force_login(self.good_user)
+
         # Load the local test Excel file
         with open('static/testing/.xlsx', 'rb') as f:
             excel_file = SimpleUploadedFile(
@@ -118,31 +122,39 @@ class TestAnalytics(TestCase):
 
         # Simulate a POST request with the Excel file
         response = self.client.post(
-            reverse('add_report'),  # Replace with the correct view name
+            reverse('add_report'),
             {'excel_report': excel_file, 'project': self.project.pk, 'name': 'Good report'},  # File passed in the POST data
             format='multipart'
         )
 
-        self.assertEqual(response.status_code, 302) # Redirect on failure
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('view_all_reports'))
-        self.assertEqual(AutodeskConstructionCloudReport.objects.count(), 2) # Now two reports should exist
+        self.assertEqual(AutodeskConstructionCloudReport.objects.count(), 2)
         self.assertTrue(AutodeskConstructionCloudReport.objects.filter(project=self.project.pk, name='Good report').exists())
         
-        self.client.logout()
+    def test_add_report_post_bad(self):
         view_permission = Permission.objects.get(codename='view_autodeskconstructioncloudreport', content_type__app_label='analytics')
         self.bad_user.user_permissions.add(view_permission)
         self.client.force_login(self.bad_user)
 
+        # Load the local test Excel file
+        with open('static/testing/.xlsx', 'rb') as f:
+            excel_file = SimpleUploadedFile(
+                ".xlsx",    # File name
+                f.read(),   # File content
+                content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+
         # Simulate a POST request with the Excel file
         response = self.client.post(
-            reverse('add_report'),  # Replace with the correct view name
+            reverse('add_report'),
             {'excel_report': excel_file, 'project': self.project.pk, 'name': 'Bad report'},  # File passed in the POST data
             format='multipart'
         )
 
-        self.assertEqual(response.status_code, 302) # Redirect on failure
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('view_all_reports'))
-        self.assertEqual(AutodeskConstructionCloudReport.objects.count(), 2) # Still only two reports should exist
+        self.assertEqual(AutodeskConstructionCloudReport.objects.count(), 2)
         self.assertFalse(AutodeskConstructionCloudReport.objects.filter(project=self.project.pk, name='Bad report').exists())
 
     def test_update_project(self):
@@ -150,6 +162,9 @@ class TestAnalytics(TestCase):
         response = self.client.get(reverse('update_project', kwargs={'project_pk': self.project.pk}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Project")
+    
+    def test_update_project_post(self):
+        self.client.force_login(self.good_user)
         response = self.client.post(reverse('update_project', kwargs={'project_pk': self.project.pk}), {'name': 'Updated Project', 'org': self.project.org})
         self.assertEqual(response.status_code, 302)
         self.project.refresh_from_db()
@@ -160,6 +175,9 @@ class TestAnalytics(TestCase):
         response = self.client.get(reverse('update_report', kwargs={'report_pk': self.report.pk}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Report")
+    
+    def test_update_report_post(self):
+        self.client.force_login(self.good_user)
         response = self.client.post(reverse('update_report', kwargs={'report_pk': self.report.pk}), {'name': 'Updated Report', 'project': self.report.project.pk, 'data': self.report.data})
         self.assertEqual(response.status_code, 302)
         self.report.refresh_from_db()
@@ -171,6 +189,13 @@ class TestAnalytics(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('view_all_projects'))
         self.assertFalse(AutodeskConstructionCloudProject.objects.filter(pk=self.project.pk).exists())
+    
+    def test_delete_project_bad(self):
+        self.client.force_login(self.bad_user)
+        response = self.client.post(reverse('delete_project', kwargs={'project_pk': self.project.pk}))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('view_all_projects'))
+        self.assertTrue(AutodeskConstructionCloudProject.objects.filter(pk=self.project.pk).exists())
 
     def test_delete_report(self):
         self.client.force_login(self.good_user)
@@ -178,3 +203,10 @@ class TestAnalytics(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('view_all_reports'))
         self.assertFalse(AutodeskConstructionCloudReport.objects.filter(pk=self.report.pk).exists())
+    
+    def test_delete_report_bad(self):
+        self.client.force_login(self.bad_user)
+        response = self.client.post(reverse('delete_report', kwargs={'report_pk': self.report.pk}))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('view_all_reports'))
+        self.assertTrue(AutodeskConstructionCloudReport.objects.filter(pk=self.report.pk).exists())
